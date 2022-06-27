5ff1f453621d930c322bb7bfb6fb31fd8eee20f6
jathom30 <jathom30@gmail.com>
2022-06-27 16:40:13 -0400

    working password reset

--- src/routes/AccountRecoveryRoute/AccountRecoveryRoute.tsx
+++ src/routes/AccountRecoveryRoute/AccountRecoveryRoute.tsx
@@ -1,31 +1,81 @@
-import { FlexBox } from "components";
-import React, { useEffect } from "react";
-import { useIdentityContext } from "react-netlify-identity";
-import { useQuery } from "react-query";
+import React, { MouseEvent, useState } from "react";
+import { Button, FlexBox, Input, Loader } from "components";
+import { useMutation, useQuery } from "react-query";
+import GoTrue from 'gotrue-js'
+import { useLocation, useNavigate } from "react-router-dom";
+import './AccountRecoveryRoute.scss'
+import { Chair } from "assets";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+
+const auth = new GoTrue({
+  APIUrl: 'https://salon-contacts.netlify.app/.netlify/identity',
+  audience: '',
+  setCookie: false,
+})
 
 export const AccountRecoveryRoute = () => {
-  const {recoverAccount, param, user} = useIdentityContext()
-  const {token} = param
+  const location = useLocation()
+  const navigate = useNavigate()
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  
+  const recoveryToken = location.hash.replace(/#recovery_token=/, '')
 
-  useEffect(() => {
-    console.log(param)
-  }, [param])
-
-  useQuery(['recover', token], () => recoverAccount(), {
-    enabled: !!param.token,
-    onSuccess: () => {
-      console.log(user)
+  useQuery(['recover'], () => auth.recover(recoveryToken), {
+    enabled: !!recoveryToken,
+    onSuccess: (data) => {
+      // setUser(data)
+    },
+    onError: (err) => {
+      console.error(err)
     }
   })
 
+  const updatePasswordMutation = useMutation(async () => auth.currentUser()?.update({ password }), {
+    onSuccess: () => {
+      navigate('/login')
+    }
+  })
+
+  const handleSavePassword = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    updatePasswordMutation.mutate()
+  }
+
+  const isValidPassword = password.length > 0 && (password === confirmPassword)
+
   return (
     <div className="AccountRecoveryRoute">
-      <FlexBox flexDirection="column" gap="1rem" alignItems="center" padding="2rem">
-        <h1>Password recovery</h1>
-        {token || 'none'}
-          {/* <p>Following the link below will log you in and redirect you to the account settings page where you will have the opportunity to update your password and any other user related settings.</p>
-          <Button type="submit"  onClick={() => recoverAccountQuery.refetch()}>Go to Account settings</Button> */}
-      </FlexBox>
+      {!auth.currentUser() ? (
+        <FlexBox flexDirection="column" gap="1rem" alignItems="center" padding="2rem">
+          <h1>Loading your account...</h1>
+          <Loader size="l" />
+        </FlexBox>
+      ) : (
+        <>
+          <div className="AccountRecoveryRoute__logo">
+            <Chair />
+          </div>
+          <h1>Hey {auth.currentUser()?.user_metadata.firstName},</h1>
+          <p>Once you update your password below, you'll be taken to the log in screen to use your fresh credentials.</p>
+          <form action="submit">
+            <FlexBox flexDirection="column" gap="1rem" padding="1rem">
+              {/* <h3>Update your password</h3> */}
+              <Input type="password" label="Password" value={password} onChange={setPassword} name="password" />
+              <Input type="password" label="Verify Password" value={confirmPassword} onChange={setConfirmPassword} name="verify-password" />
+              <Button
+                icon={faSave}
+                type="submit"
+                kind="primary"
+                onClick={handleSavePassword}
+                isDisabled={!isValidPassword || updatePasswordMutation.isLoading}
+              >
+                {updatePasswordMutation.isLoading ? <Loader /> : 'Save new password'}
+              </Button>
+            </FlexBox>
+          </form>
+        </>
+      )}
     </div>
   )
 }
